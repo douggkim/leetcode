676591269b4cd0dcb530651f8912edcf6f27d1fa
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-10-12 22:24:01 -0700

    Time: 99 ms (8.07%), Space: 14 MB (24.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -20,19 +20,5 @@ class Solution:
                 break
             if i==0 :
                 nums.sort()
-                # temp = nums[0]
-                # nums[0] = nums[len(nums)-1] 
-                # nums[len(nums)-1] = temp 
-                # nums[1:]=sorted(nums[1:])
-                # print(nums)
-                # print(front)
-                # nums_part = nums[:-1]
-                # nums_part.sort()
-                # print(nums_part)
-                # front.extend(nums_part)
-                # print(front)
-                # nums=front
-                # nums.sort()
-                # print(f"front:{nums}")
                 break
             
