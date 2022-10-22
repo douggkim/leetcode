820347735db31dd56e48f809f1cac0a703894179
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-10-22 11:41:27 -0700

    Time: 310 ms (96.33%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,18 +1,17 @@
-# Write your MySQL query statement below
-WITH OT AS 
-(SELECT C.visited_on, SUM(amount) AS amount, ROW_NUMBER() OVER (ORDER BY visited_on) rownum
+WITH NT AS 
+(
+SELECT C.visited_on, SUM(C.amount) AS amt, AVG(C.amount) AS avg_amt, ROW_NUMBER() OVER (ORDER BY C.visited_on) AS rownum 
 FROM Customer C
-GROUP BY 1
-ORDER BY rownum), OT2 AS 
-(SELECT C.visited_on AS visited_on, SUM(amount) OVER ( ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount, ROUND(AVG(amount) OVER ( ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ),2) AS average_amount, rownum
-FROM OT C 
-WHERE amount IS NOT NULL
-ORDER BY visited_on)
+GROUP BY 1),
+NT2 AS 
+(
+SELECT visited_on, SUM(amt) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount, ROUND(AVG(amt) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2) AS average_amount, rownum
+FROM NT 
+)
 
-SELECT C.visited_on, C.amount, C.average_amount 
-FROM OT2 C 
-WHERE C.rownum >=7
-
+SELECT visited_on, amount, average_amount
+FROM NT2 
+WHERE rownum >= 7
 
 
 
