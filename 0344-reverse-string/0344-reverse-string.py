5f5d389a4e5eabd64f7882ed4e46a454d382a349
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-08 10:04:25 -0800

    Time: 422 ms (63.58%), Space: 18.5 MB (19.21%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,16 +1,13 @@
 from copy import deepcopy
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        length = len(s)
-        for i in range(length-1,-1,-1): 
-            s.append(s[i])
-            
-        for i in range(length):
-            s.pop(0)
+        l = 0 
+        r = len(s)-1 
         
+        while l <= r : 
+            s[l],s[r] = s[r],s[l]
+            l += 1 
+            r -= 1
             
         
         
