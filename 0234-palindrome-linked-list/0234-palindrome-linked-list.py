a36eafd73fa27b545e18e1b2b9e833e2f03f54d9
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-14 10:08:23 -0800

    Time: 2063 ms (24.65%), Space: 46.6 MB (46.33%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -10,48 +10,18 @@ class Solution:
         # count -> even, odd -> cases -> pop stack 
         # Get the length 
         ptr = head 
-        ctr = 1 
-        while ptr.next != None: 
+        l = [head.val] 
+        while ptr.next != None:
             ptr = ptr.next 
-            ctr += 1 
+            l.append(ptr.val)
         
-        ptr = head 
-        new_ctr = 1 
-        st = [] 
-        
-        if ctr % 2 ==0 : 
-            while ptr != None : 
-                if new_ctr >= (ctr//2+1): 
-                    if ptr.val == st.pop():
-                        ptr = ptr.next 
-                        
-                    else: 
-                        return False
-                else: 
-                    st.append(ptr.val)
-                    ptr = ptr.next 
-                    new_ctr += 1
-        
-        else : 
-            while ptr != None: 
-                if new_ctr == (ctr//2 +1):
-                    ptr = ptr.next
-                    new_ctr += 1 
-                elif new_ctr > (ctr//2 + 1): 
-                    if ptr.val == st.pop():
-                        ptr = ptr.next 
-                        
-                    else: 
-                        return False 
-                else:
-                    st.append(ptr.val)
-                    ptr = ptr.next
-                    new_ctr += 1
-        
-        if len(st) == 0: 
-            return True
-        else: 
+        if l == l[::-1]: 
+            return True 
+        else:
             return False
+            
+        
+        
         
         
         # if even 
