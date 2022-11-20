82044485d88bd4fc32862997e01a5ecf2039b717
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-19 22:07:31 -0800

    Time: 64 ms (68.69%), Space: 13.9 MB (80.13%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -14,21 +14,16 @@ class Solution:
         elif list1 != None and list2 == None: 
             return list1 
         
-        # start with list 1 if smaller 
         if list1.val <= list2.val: 
             head = list1 
             curr_1 = list1.next 
             curr_2 = list2
-            # head = head.next 
-            # head.next = None 
-        # start with list 2 if smaller 
+        
         else: 
             head = list2 
             curr_2 = list2.next 
             curr_1 = list1
-            # head = head.next 
-            # head.next = None 
-            print(head)
+
         
         start = head 
         
@@ -38,16 +33,13 @@ class Solution:
                 head.next = curr_1
                 head = head.next 
                 curr_1 = curr_1.next 
-                
-                # head.next = None
+            
                 
             else:
                 head.next = curr_2 
                 head = head.next 
                 curr_2 = curr_2.next 
 
-                # head.next = None 
-            # print(head) 
             
         if curr_1 != None: 
             head.next = curr_1 
