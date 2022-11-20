136a541c0887be7c30aa22f2bdf8f87fbd178dfa
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-20 13:41:07 -0800

    Time: 1152 ms (71.13%), Space: 14.2 MB (26.98%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,20 +1,18 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        result_dict = {} 
+        result_dict = defaultdict(int)
         cnt = 0 
         n = len(nums1)
         
         for i in range(n):
             for j in range(n): 
-                if (nums1[i]+nums2[j]) not in result_dict: 
-                    result_dict[nums1[i]+nums2[j]] = 1
-                else: 
-                    result_dict[nums1[i]+nums2[j]] += 1 
+                
+                result_dict[nums1[i]+nums2[j]] += 1 
         
         for i in range(n):
             for j in range(n):
-                if -(nums3[i]+nums4[j]) in result_dict: 
-                    cnt += result_dict[-(nums3[i]+nums4[j])]
+                
+                cnt += result_dict[-(nums3[i]+nums4[j])]
                     
         return cnt 
                     
