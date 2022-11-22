1c3ca3ffbf08e568dfed6260ca40f187cb5a33ae
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-22 13:27:38 -0800

    Time: 2432 ms (54.05%), Space: 24.9 MB (86.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # loop through the prices 
-        # select a beg day 
-        # select a end - day from beg+1 
-        # subtract 
-        # if larger than 0 
-        # save results to a list 
-        if len(prices) == 0: 
-            return 0 
-        
-        min_price = 100000000
-        max_prof = 0 
+        min_p = 100001
+        max_p = 0 
         
         for i in range(len(prices)): 
-            if prices[i] < min_price: 
-                min_price = prices[i]
-            elif min_price < prices[i]:
-                max_prof = max(prices[i]-min_price,max_prof) 
-            
-        return max_prof
-    
-        #Edge cases: 
+            if prices[i] < min_p : 
+                min_p = prices[i]
+            elif min_p < prices[i]: 
+                max_p = max(max_p, prices[i]-min_p)
+        
+        return max_p
+        
