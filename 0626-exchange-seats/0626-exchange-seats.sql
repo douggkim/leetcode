5eb03fd929da92778473896ba5a6eb5105839dc2
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-22 10:48:44 -0800

    Time: 586 ms (16.58%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-SELECT id,CASE WHEN id=(SELECT COUNT(*) FROM Seat S3) AND id%2!=0 THEN S1.student 
-WHEN id%2=0 THEN (SELECT student FROM Seat S3 WHERE S3.id=S1.id-1) WHEN id%2!=0 THEN (SELECT student FROM Seat S3 WHERE S3.id=S1.id+1) END AS student
+
+
+SELECT id, CASE 
+WHEN (id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat)) THEN student
+WHEN (S1.id % 2 = 1) THEN (SELECT student FROM Seat S2 WHERE S2.id = S1.id +1)
+WHEN (S1.id % 2 = 0) THEN (SELECT student FROM Seat S2 WHERE S2.id = S1.id - 1)
+END  AS student
 FROM Seat S1
