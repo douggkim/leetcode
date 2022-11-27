6bfe59ecc23cacb3f2d4f4799f06f68ca0f29b2c
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2022-11-26 16:17:18 -0800

    Time: 335 ms (71.77%), Space: 0B (100.00%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# rank based on the scores (dense ranks) - DESC score 
+# Score DESC 
+# score / rank 
+# ROUND(,2) -> maintain
+
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
+FROM Scores 
+ORDER BY score DESC
