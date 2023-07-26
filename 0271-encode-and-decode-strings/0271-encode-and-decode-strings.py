ea9ffe83b6713506a1db642daf2f600cb1f28737
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-07-26 12:58:51 -0700

    Time: 326 ms (5.71%), Space: 16.8 MB (37.54%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,37 +2,34 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        encoded_str = "" 
+        encoded = "" 
         
         for s in strs: 
-            encoded_str += s.replace('/','//') + "/:"
+            encoded += (s.replace("/", "//") +"/:") 
         
-        return encoded_str 
+        return encoded 
+        
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        decoded_str = [] 
-        
-        decoded_word = "" 
+        tmp_word = "" 
+        decoded_l = [] 
         i = 0 
         
         while i < len(s): 
-            if s[i:i+2] == '//': 
-                decoded_word += '/'
+            if s[i:i+2] == '/:':
+                decoded_l.append(tmp_word) 
+                tmp_word = "" 
                 i += 2 
-                
-            elif s[i:i+2] == '/:': 
-                decoded_str.append(decoded_word) 
-                decoded_word = ""
-                i += 2                 
-                
+            elif s[i:i+2] == '//': 
+                tmp_word += "/"
+                i += 2 
             else: 
-                decoded_word += s[i] 
-                i += 1 
-        
-        return decoded_str
-            
+                tmp_word += s[i]
+                i += 1
+                
+        return decoded_l 
         
 
 
