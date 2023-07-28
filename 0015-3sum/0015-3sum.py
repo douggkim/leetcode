3120d246846f3b6daf14a142822715ed3472f343
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-07-28 14:47:53 -0700

    Time: 1106 ms (88.66%), Space: 21.2 MB (14.19%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        r = []
-        nums.sort()
+        sorted_nums = sorted(nums)
+        i = 0
+        result_l = [] 
+        while i < len(sorted_nums): 
+            if sorted_nums[i] > 0: 
+                break
+            if  i ==0 or sorted_nums[i] != sorted_nums[i-1]: 
+                result_l = self.twoSum(sorted_nums[i:], result_l)
+            i += 1 
         
-        for i in range(len(nums)):
-            if nums[i] >0 : 
-                break 
-            if i == 0 or nums[i-1] != nums[i]:
-                self.twosum(nums, i, r)
-        return r 
-        
-    def twosum(self, nums,start,r): 
-        result = set() 
-        i = start+1
-        while i < len(nums):
-            tmp = nums[start] + nums[i]
-            if -1*tmp in result: 
-                r.append([nums[i],nums[start],-1*tmp])
-                while i +1 < len(nums) and nums[i] == nums[i+1]:
-                    i += 1 
-
-            result.add(nums[i])
-            i+=1
+        return result_l 
                 
+        
+    def twoSum(self, nums: List[int], result_l: List[List[int]]) -> List[List[int]]: 
+        fixed_num = nums[0]
+        
+        i = 1
+        save = set()
+
+        while i < len(nums):
+            tmp_sum = nums[i] + fixed_num 
+            if -tmp_sum in save: 
+                result_l.append([nums[i], fixed_num, -tmp_sum])
+                while i < len(nums)-1 and nums[i] == nums[i+1]:
+                        i += 1 
+            
+            save.add(nums[i])
+            i += 1 
+            
+        return result_l 
+            
+            
+                 
                     
                 
-                
-        
-            
-        
-        return r
-                        
-                
-        
