12dfe4eb4fff7a66f85861adebeca4ba38a12eee
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-07-28 17:00:41 -0700

    Time: 1097 ms (89.44%), Space: 21.2 MB (14.19%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        sorted_nums = sorted(nums)
-        i = 0
+        sorted_num = sorted(nums) 
         result_l = [] 
-        while i < len(sorted_nums): 
-            if sorted_nums[i] > 0: 
-                break
-            if  i ==0 or sorted_nums[i] != sorted_nums[i-1]: 
-                result_l = self.twoSum(sorted_nums[i:], result_l)
-            i += 1 
+        
+        for i in range(len(sorted_num)): 
+            if sorted_num[i] > 0:
+                break 
+            if i==0 or sorted_num[i]!= sorted_num[i-1]: 
+                result_l = self.twoSum(sorted_num[i:], result_l)
         
         return result_l 
-                
         
     def twoSum(self, nums: List[int], result_l: List[List[int]]) -> List[List[int]]: 
-        fixed_num = nums[0]
+        start = nums[0]
         
         i = 1
-        save = set()
-
-        while i < len(nums):
-            tmp_sum = nums[i] + fixed_num 
-            if -tmp_sum in save: 
-                result_l.append([nums[i], fixed_num, -tmp_sum])
-                while i < len(nums)-1 and nums[i] == nums[i+1]:
-                        i += 1 
+        visited = set() 
+        while i < len(nums): 
+            tmp_val = nums[i] + start 
+            if -tmp_val in visited: 
+                result_l.append([start, nums[i], -tmp_val])
+                while i < len(nums)-1 and nums[i] == nums[i+1]: 
+                    i += 1 
             
-            save.add(nums[i])
+            visited.add(nums[i])
             i += 1 
             
         return result_l 
-            
+        
+        
             
                  
                     
