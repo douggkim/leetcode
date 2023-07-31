ab3049092bb58bad7f861502870160c8905c91e5
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-07-30 21:43:30 -0700

    Time: 289 ms (7.92%), Space: 16.8 MB (61.74%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,16 +2,41 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        encoded = "ㄱ".join(strs) 
-        return encoded 
+        encoded = "" 
         
+        for s in strs: 
+            for i in range(len(s)): 
+                if s[i] == '/': 
+                    encoded+='//'
+                else: 
+                    encoded+=s[i]
+            encoded += '/:'
+        
+        return encoded 
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        decoded = s.split("ㄱ")
+        result_l = []
+        tmp_word = "" 
+        i = 0 
         
-        return decoded 
+        while i < len(s): 
+            if s[i:i+2] == '//': 
+                tmp_word += '/'
+                i += 2 
+            elif s[i:i+2] == '/:': 
+                result_l.append(tmp_word)
+                i += 2 
+                tmp_word = "" 
+            else: 
+                tmp_word += s[i]
+                i += 1 
+        
+        return result_l
+            
+            
+        
         
 
 
