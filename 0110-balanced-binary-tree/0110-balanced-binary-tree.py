ce8ce6b9392bd95b03d7482e76b74ae92ef134df
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-01 16:27:43 -0700

    Time: 55 ms (93.05%), Space: 21.6 MB (7.64%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,17 +6,32 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root is None: 
-            return -1 
-        return abs(self.checkheight(root.left)-self.checkheight(root.right))<2 and self.isBalanced(root.left) and self.isBalanced(root.right) 
-    def checkheight(self, root: Optional[TreeNode]) -> int: 
-        if root is None: 
-            return -1 
-        return 1+max(self.checkheight(root.left),self.checkheight(root.right))
-
-         
+        if root == None: 
+            return True
+        depth, isBalanced = self.search(root,0,True)
+        
+        return isBalanced
+    
+    def search(self, node:TreeNode, depth:int, isBalanced:bool) -> tuple[int,bool]: 
+        depth_l = depth 
+        depth_r = depth 
+        isBalanced_l = True 
+        isBalanced_r = True 
+        
+        if node.left != None: 
+            depth_l, isBalanced_l = self.search(node.left, depth+1, isBalanced)
+        if node.right != None: 
+            depth_r, isBalanced_r = self.search(node.right, depth+1, isBalanced)
             
-            
+        currDepth_l = depth_l - depth
+        currDepth_r = depth_r - depth 
         
+        if abs(currDepth_l - currDepth_r) > 1: 
+            isBalanced = False
         
+        isBalanced = isBalanced and isBalanced_r and isBalanced_l 
         
+        tmp_depth = max(depth_l, depth_r)
+        depth = max(tmp_depth, depth)
+        
+        return depth, isBalanced
