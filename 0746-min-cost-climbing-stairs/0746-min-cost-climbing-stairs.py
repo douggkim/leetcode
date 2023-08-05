a6fd816baefbdad0fa00f5ed6a84b96daf3ac497
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-04 18:54:50 -0700

    Time: 59 ms (94.24%), Space: 16.4 MB (89.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,29 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        steps = [1,2]
-        # EC : 0 length 
-        # EC : 1 length 
+        record_l = [0] * (len(cost)+1)
         
-        # Start from 0 
-        rec = [0]*(len(cost)+1) 
-        for i in range(1, len(cost)+1): 
-            if i == 1:
-                rec[i] += cost[i-1]
-            else:
-                rec[i] = min(rec[i-1]+cost[i-1],rec[i-2]+cost[i-2])
-        tmp1 = rec[len(cost)]
-        print(rec)
+        for i in range(2,len(record_l)): 
+            tmp_result_1 = record_l[i-2] + cost[i-2]
+            tmp_result_2 = record_l[i-1] + cost[i-1]
+            record_l[i] = min(tmp_result_1, tmp_result_2)
         
-        rec = [0]*(len(cost)+1)
-        for i in range(2,len(cost)+1):
-            if i == 2: 
-                rec[i]+=cost[i-1]
-            else: 
-                rec[i] = min(rec[i-1]+cost[i-1], rec[i-2]+cost[i-2])
-        tmp2 = rec[len(cost)]
-        print(rec)
-        
-        return min(tmp1,tmp2)
-            
-
-    # start from 1
+        return record_l[len(cost)]
