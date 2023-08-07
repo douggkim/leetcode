ece0587512a4bc32b952e12cde10eb331e6c5518
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-07 13:27:04 -0700

    Time: 80 ms (49.86%), Space: 16.4 MB (31.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,65 +5,38 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # Set cursors 
-        l1_curr = l1
-        l2_curr = l2
+        l1_carry = "" 
+        tmp_l1 = l1 
         
-        # define a return node 
-        return_n = ListNode() 
-        return_start = return_n
+        while tmp_l1 != None: 
+            l1_carry += str(tmp_l1.val)
+            tmp_l1 = tmp_l1.next 
         
-        # define a number to contain digits of ten from result 
-        pass_num = 0 
+        l1_reverse = l1_carry[::-1]
+        l1_carry_int = int(l1_reverse)
         
+        l2_carry = "" 
+        tmp_l2 = l2
         
+        while tmp_l2 != None: 
+            l2_carry += str(tmp_l2.val)
+            tmp_l2 = tmp_l2.next 
         
-        while l1_curr != None and l2_curr != None: 
-            #add the two digits 
-            result_num = l1_curr.val + l2_curr.val
-            result_num += pass_num 
-            
-            return_n.val = result_num%10
-            pass_num = result_num//10
-            
-            # move to next 
-            l1_curr = l1_curr.next 
-            l2_curr = l2_curr.next
-            if l1_curr != None or l2_curr != None: 
-                return_n.next  = ListNode()
-                return_n = return_n.next
-            
-            
-        while l1_curr != None: 
-            result_num = l1_curr.val + pass_num 
-            
-            return_n.val = result_num%10 
-            pass_num = result_num//10 
-            
-            # move to next 
-            l1_curr = l1_curr.next 
-            if l1_curr != None: 
-                return_n.next  = ListNode()
-                return_n = return_n.next
-            
-        while l2_curr != None: 
-            result_num = l2_curr.val + pass_num 
-             
-            return_n.val = result_num%10 
-            pass_num = result_num//10 
-            
-            # move to next 
-            l2_curr = l2_curr.next 
-            if l2_curr != None: 
-                return_n.next  = ListNode()
-                return_n = return_n.next
-            
-    
-        if pass_num != 0: 
-            return_n.next = ListNode() 
-            return_n = return_n.next 
-            return_n.val = pass_num
+        l2_reverse = l2_carry[::-1]
+        l2_carry_int = int(l2_reverse)
         
-        return return_start
-            
+        result = l1_carry_int + l2_carry_int 
+        result_str = str(result)
+        result_l = [c for c in result_str]
+        result_l = result_l[::-1]
         
+        head = ListNode(int(result_l[0])) 
+        result_tmp = head 
+        
+        for i in range(1, len(result_l)): 
+            result_tmp.next = ListNode(int(result_l[i]))
+            result_tmp = result_tmp.next
+        
+        return head 
+        
+            
