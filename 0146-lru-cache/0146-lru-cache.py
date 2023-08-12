9c6e579e860ad99f1425a4b2cd431d9e22bc3101
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-12 13:11:56 -0700

    Time: 717 ms (74.36%), Space: 78.5 MB (41.22%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,56 +1,58 @@
 class Node: 
     def __init__(self, key, val): 
-        self.key = key
+        self.key = key 
         self.val = val 
         self.next = None 
-        self.prev = None 
-        
-class LRUCache:
+        self.prev = None
 
+class LRUCache:
     def __init__(self, capacity: int):
-        self.capa = capacity
+        self.capa = capacity 
         self.dic = {} 
         self.head = Node(-1,-1)
         self.tail = Node(-1,-1)
         self.head.next = self.tail 
-        self.tail.prev = self.head
-        
+        self.tail.prev = self.head 
 
     def get(self, key: int) -> int:
         if key not in self.dic: 
             return -1 
-
+        
         node = self.dic[key]
+
         self.remove(node)
         self.add(node)
-        return node.val 
 
+        return node.val
+        
     def put(self, key: int, value: int) -> None:
         if key in self.dic: 
-            old_node = self.dic[key]
-            self.remove(old_node)
+            self.remove(self.dic[key])
         
-        node = Node(key,value)
-        self.dic[key] = node 
-        self.add(node)
+        new_node = Node(key,value)
+        self.dic[key] = new_node 
+        self.add(new_node)
 
         if len(self.dic) > self.capa: 
-            previous = self.head.next 
-            self.remove(previous)
-            del self.dic[previous.key]
+            delete_node = self.head.next 
+            self.remove(delete_node)
+            del self.dic[delete_node.key]
             
-
+            
+        
     
     def add(self, node): 
         previous = self.tail.prev 
         previous.next = node 
-        node.prev = previous
-        node.next = self.tail
         self.tail.prev = node 
-
+        node.prev = previous
+        node.next = self.tail 
+    
     def remove(self, node): 
-        node.prev.next = node.next 
+        previous = node.prev 
+        previous.next = node.next
         node.next.prev = node.prev 
+
 
         
 
