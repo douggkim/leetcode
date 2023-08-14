873cc7d2a89c955c0473e27b76a74595ea4a6057
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-13 17:50:56 -0700

    Time: 37 ms (93.33%), Space: 16.6 MB (62.59%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        current_list = []     
-        def parenthesis_helper( n:int, left:int, right:int, current_str:str) -> None: 
-            if left == right and left + right == n*2: 
-                current_list.append(current_str)
-            else: 
-                if left < n: 
-                    parenthesis_helper(n=n, left=left+1, right=right, current_str= current_str+"(")
-                if right < left: 
-                    parenthesis_helper(n=n, left=left, right=right+1, current_str= current_str+")")
+        current_l = [] 
         
-        parenthesis_helper(n,left=1,right=0,current_str="(")
-
-        return current_list
+        def helper(left:int, right:int, paren:str) : 
+            if left==right and left+right == 2*n : 
+                current_l.append(paren)
+            
+            if left < n: 
+                helper(left+1, right, paren+"(")
+            if right < left: 
+                helper(left, right+1, paren+")")
+        
+        helper(1,0,"(")
+        
+        return current_l 
+            
             
         
         
