96ac7bb3e5a48acbe332c4cb39eabb4bc2448fb2
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-20 14:18:10 -0700

    Time: 561 ms (78.33%), Space: 70.5 MB (21.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,20 +2,22 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
         
-        if total % 2 != 0 : 
+        if total % 2 != 0: 
             return False 
         
-        subset_sum = total//2 
+        subtotal = total //2 
         
         n = len(nums)
         
         @cache 
-        def dfs(nums:Tuple[int], n:int, subset_sum:int) -> bool: 
+        def dfs(nums:tuple[int], n:int, subset_sum:int) -> bool: 
             if subset_sum == 0: 
-                return True 
-            if n == 0 or subset_sum < 0: 
+                return True
+            
+            if n == 0 or subset_sum < 0 : 
                 return False 
             
-            return (dfs(nums,n-1,subset_sum-nums[n-1]) or dfs(nums,n-1, subset_sum))
+            return (dfs(nums, n-1, subset_sum-nums[n-1]) 
+                   or dfs(nums, n-1, subset_sum)) 
         
-        return dfs(tuple(nums), n-1, subset_sum)
+        return dfs(tuple(nums), n-1, subtotal)
