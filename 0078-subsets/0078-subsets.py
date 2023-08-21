de8b908930a71caf83214aa007cfc6369bc4169e
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-21 14:50:35 -0700

    Time: 37 ms (89.23%), Space: 16.4 MB (95.57%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(k, first=0, curr=[]): 
+            if len(curr) == k: 
+                result_l.append(curr[:])
+                return 
+            
+            for i in range(first,n): 
+                curr.append(nums[i])
+                backtrack(k, i+1, curr)
+                curr.pop()
+                
         result_l = [] 
-        
-        def dfs(curr_set, nums, max_idx, result_l, flag)-> List[List[int]]: 
-            if max_idx < -1: 
-                return result_l
-            if flag == 0: 
-                result_l.append(curr_set)
-            new_curr_set = curr_set.copy()
-            result_l = dfs(new_curr_set, nums,max_idx-1, result_l, 1)
+        n = len(nums)
+        for j in range(n+1): 
+            backtrack(k=j)
             
-            new_curr_set.append(nums[max_idx])
-            new_curr_set_2 = new_curr_set.copy()
-            result_l = dfs(new_curr_set_2, nums,max_idx-1, result_l, 0)
+        return result_l 
             
-            return result_l
         
-        result_l = dfs([],nums,len(nums)-1, result_l, 0)
-        
-        return result_l
