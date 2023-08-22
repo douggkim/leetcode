6be297926670bd3bc75ee2a621f7437f3463d153
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-08-22 13:16:10 -0700

    Time: 37 ms (95.00%), Space: 16.6 MB (36.44%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,23 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        record_l = [0]*n
-        result_l = [] 
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
         
-        def dfs(tmp_l, record_l, total): 
-            if total == n:
-                result_l.append(tmp_l)
-            if total > n:
-                return 
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
             
-            for i,v in enumerate(record_l): 
-                if v == 0: 
-                    tmp = record_l.copy()
-                    tmp[i] = 1 
-                    dfs(tmp_l+[nums[i]], tmp, total+1)
-        
-        dfs([],record_l,0)
-        
-        return result_l
-                
-            
+        ans = []
+        backtrack([])
+        return ans
