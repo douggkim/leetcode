cedc8181d62300da3090d016b1b13154c464f7a7
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-09-17 04:10:07 +0900

    Time: 301 ms (56.06%), Space: 19.5 MB (48.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,43 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n, m = len(grid), len(grid[0])
-        result = [["0"]*m for i in range(n)] 
+        m = len(grid)
+        n = len(grid[0])
+        
+        record_l = [[0 for i in range(n)] for j in range(m)]
         cnt = 0 
         
-        def bfs(i:int, j:int, grid:List[List[str]], result:List[List[str]])-> List[List[str]]:
-            result[i][j] = '1' 
-            ip1 = i+1 
-            jp1 = j+1 
-            im1 = i-1
-            jm1 = j-1 
-            if ip1< len(grid) and grid[ip1][j] == '1' and result[ip1][j] == '0': 
-                # print(f"{i} / {j} triggered")
-                result = bfs(ip1,j,grid,result)
-            if jp1< len(grid[0]) and grid[i][jp1] == '1' and result[i][jp1] == '0': 
-                # print(f"{i} / {j} triggered")
-                result = bfs(i,jp1,grid,result)
-            if im1 >= 0 and grid[im1][j]=='1' and result[im1][j] == '0':
-                # print(f"{i} / {j} triggered")
-                result = bfs(im1,j,grid,result)
-            if jm1 >= 0 and grid[i][jm1]=='1' and result[i][jm1] == '0':
-                # print(f"{i} / {j} triggered")
-                result = bfs(i,jm1,grid,result)
+        def traverse(i:int, j:int): 
+            move = [[-1,0],[0,-1],[1,0],[0,1]]
             
-            return result
+            for _ in move: 
+                
+                new_i = i+_[0]
+                new_j = j +_[1] 
+                if (new_i< m and 0 <= new_i) and (new_j<n and 0<=new_j) and grid[new_i][new_j] == "1" and record_l[new_i][new_j] != 1: 
+                    record_l[new_i][new_j] = 1 
+                    traverse(new_i, new_j)
         
-        for i in range(n):
-            for j in range(m): 
-                if grid[i][j] == '1' and result[i][j]=='0':
-                    # print(f"dfs triggererd cnt: {cnt+1}")
+        for i in range(m): 
+            for j in range(n): 
+                if grid[i][j] != "0" and record_l[i][j] != 1: 
                     cnt += 1 
-                    result = bfs(i,j,grid,result)
-                    # print(result)
-                result[i][j] = '1'
-        
+                    traverse(i,j)
+                    
+                    
         
         return cnt
-            
-            
                     
-            
-            
