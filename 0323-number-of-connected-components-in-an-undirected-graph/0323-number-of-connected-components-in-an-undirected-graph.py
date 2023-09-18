e0ec6758ac03c6dac0ed4a7ce3f91b6ed0d67104
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-09-17 17:07:14 -0700

    Time: 95 ms (86.69%), Space: 20.2 MB (6.63%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,33 +1,35 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        edge_dict = {}
+        edge_dict = {} 
         
-        for edge in edges:
-            if edge[0] in edge_dict:
+        for edge in edges: 
+            if edge[0] in edge_dict: 
                 edge_dict[edge[0]].append(edge[1])
-            else:
+            else: 
                 edge_dict[edge[0]] = [edge[1]]
                 
-            if edge[1] in edge_dict:
+            if edge[1] in edge_dict: 
                 edge_dict[edge[1]].append(edge[0])
-            else:
+            else: 
                 edge_dict[edge[1]] = [edge[0]]
         
-        visited = set()
+        visited = set() 
         
-        def dfs(node):
+        def dfs(node, visited) : 
             if node in visited:
-                return
+                return 
             visited.add(node)
-            if node in edge_dict:
-                for neighbor in edge_dict[node]:
-                    dfs(neighbor)
+            
+            if node in edge_dict: 
+                neighbors = edge_dict[node]
+                for neighbor in neighbors: 
+                    dfs(neighbor, visited)
+                
+        cnt = 0 
         
-        cnt = 0
-        
-        for i in range(n):
-            if i not in visited:
-                dfs(i)
-                cnt += 1
-        
+        for i in range(n): 
+            if i not in visited: 
+                dfs(i, visited)
+                cnt += 1 
+            
         return cnt
