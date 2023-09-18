36cb73cd2d6ac94a76170014466ca7210e8b353f
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-09-17 17:45:21 -0700

    Time: 84 ms (97.34%), Space: 18.3 MB (46.82%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,17 +1,15 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         if len(edges) != n-1: 
-            return False
+            return False 
         
-        edge_dict = {} 
-        connected = set() 
+        edge_dict = {}
         
         for edge in edges: 
             if edge[0] in edge_dict: 
                 edge_dict[edge[0]].append(edge[1])
             else: 
                 edge_dict[edge[0]] = [edge[1]]
-            
             if edge[1] in edge_dict: 
                 edge_dict[edge[1]].append(edge[0])
             else: 
@@ -20,21 +18,21 @@ class Solution:
         visited = set() 
         
         def dfs(node, parent): 
-            if node in visited:
-                return 
+            if node in visited: 
+                return False
+            
             visited.add(node)
+            
             if node in edge_dict: 
-                for neighbor in edge_dict[node]: 
-                    if parent == neighbor: 
+                for neighbor in edge_dict[node]:
+                    if neighbor == parent: 
                         continue 
-                    if neighbor in visited:
-                        return False
-                    result = dfs(neighbor, node)
-                    if not result: 
+                    is_cycle = dfs(neighbor, node)
+                    if not is_cycle: 
                         return False 
-                
+            
+            
             return True 
         
-    
         
-        return dfs(0,-1) and len(visited) == n 
+        return dfs(0,-1) and len(visited) == n
