a340304733451cca7dd62dcb83d01ae07b1f3fb8
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-09-19 17:46:01 -0700

    Time: 45 ms (30.85%), Space: 16.6 MB (39.01%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(start, current):
-            # Add the current subset to the result
-            result.append(current[:])
-
-            # Explore all possible next elements in the subset
-            for i in range(start, len(nums)):
-                current.append(nums[i])
-                backtrack(i + 1, current)
-                current.pop()
-
-        result = []
-        backtrack(0, [])
-
-        return result
+        def backtrack(curr:List[int], start:int)->List[int]: 
+            result_l.append(curr.copy())
+             
+            for i in range(start, n): 
+                curr.append(nums[i])
+                backtrack(curr, i+1)
+                curr.pop()
+        
+        result_l = [] 
+        n = len(nums)
+        backtrack([],0)
+        
+        return result_l
+            
