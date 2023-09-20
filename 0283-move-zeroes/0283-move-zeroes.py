072bcdcface388c68ad351c85c57d4f86e8ce5a1
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-09-20 09:57:35 -0700

    Time: 156 ms (38.08%), Space: 17.9 MB (80.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,16 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero_cnt = 0
-        i = 0
+        i = 0 
+        zero_cnt = 0 
+        
         while i < len(nums): 
-            if nums[i] ==0 :
+            if nums[i] == 0: 
                 nums.pop(i)
-                zero_cnt+=1 
+                zero_cnt += 1 
             else: 
-                i += 1
-        
+                i += 1 
+                
         nums.extend([0]*zero_cnt)
-        print(nums)
-        return nums
         
+        return nums 
