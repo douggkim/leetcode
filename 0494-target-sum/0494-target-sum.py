8101b070ecb2ceafa091f5aa5321895393515b7f
Hyunduk Doug Kim <54276417+dougieduk@users.noreply.github.com>
2023-09-20 00:06:36 -0700

    Time: 355 ms (15.88%), Space: 17.4 MB (62.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,14 +1,16 @@
 class Solution:
     def findTargetSumWays(self, nums, S):
         total = sum(nums)
-        dp = [[0] * (2 * total + 1) for _ in range(len(nums))]
-        dp[0][nums[0] + total] = 1
-        dp[0][-nums[0] + total] += 1
-
-        for i in range(1, len(nums)):
-            for _sum in range(-total, total + 1):
-                if dp[i - 1][_sum + total] > 0:
-                    dp[i][_sum + nums[i] + total] += dp[i - 1][_sum + total]
-                    dp[i][_sum - nums[i] + total] += dp[i - 1][_sum + total]
-
-        return 0 if abs(S) > total else dp[len(nums) - 1][S + total]
+        dp = [[0]*(2*total+1) for i in range(len(nums))]
+        
+        dp[0][total+nums[0]] = 1
+        dp[0][total-nums[0]] += 1 
+        
+        for i in range(1,len(nums)): 
+            for j in range(-total, total+1): 
+                if dp[i-1][total+j]>0: 
+                    dp[i][total+j+nums[i]] += dp[i-1][total+j]
+                    dp[i][total+j-nums[i]] += dp[i-1][total+j]
+        for row in dp: 
+            print(row)
+        return 0 if abs(S) > total else dp[len(nums)-1][total+S]
